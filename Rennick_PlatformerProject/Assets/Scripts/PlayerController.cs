6073f845ebd889f1d4e8190fa3b2a25b84c0efc0
DarkAaron999 <darkco1221@outlook.com>
2024-11-22 07:23:29 -0500

    Task1 Player Movement (2hrs)

--- Rennick_PlatformerProject/Assets/Scripts/PlayerController.cs
+++ Rennick_PlatformerProject/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,14 @@ public class PlayerController : MonoBehaviour
         left, right
     }
 
+    private Rigidbody2D rb;
+    public float acceleration;
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,13 +24,21 @@ public class PlayerController : MonoBehaviour
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), 0);
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        //Vector2 movement = new Vector2 (playerInput.x * moveSpeed, rb.velocity.y);
+        //rb.velocity = movement;
 
+        Vector2 movment = new Vector2(playerInput.x * moveSpeed, rb.velocity.y);
+        rb.velocity = Vector2.Lerp(rb.velocity, movment, acceleration * Time.deltaTime);
+
+        //https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector2.Lerp.html
+        //https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Input.GetAxis.html
+        //https://stackoverflow.com/questions/32905191/c-sharp-2d-platformer-movement-code
     }
 
     public bool IsWalking()
